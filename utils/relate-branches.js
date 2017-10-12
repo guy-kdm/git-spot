cd88edbad3767c580f328706035c74e9e59bd10b
Guy Kedem <guykdm@gmail.im>
2017-10-12 15:37:50 +0300

    squash! all good

--- utils/relate-branches.js
+++ utils/relate-branches.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
+const workingDir = process.env.GIT_SPOT_CWD || '~/tmp/test-git-scripts';
+let g = require('simple-git/promise')();
 const IIFE = fn => fn();
 
 IIFE(async () => {
-  const workingDir = process.env.GIT_SPOT_CWD || '~/tmp/test-git-scripts';
-  let g = require('simple-git/promise')('');
-
+  // updating remote tracking branches
   const res = await g.fetch(['--all', '--prune']);
-  console.log(res);
+
+  const branches = await g.branchLocal();
+
+  const l = await g.log();
+  console.log(l);
 });
