edfe6919dbfb90a9e917b9162f5e940925c159f4
Guy Kedem <guykdm@gmail.im>
2017-10-12 17:15:07 +0300

    lolz

--- sync.js
+++ sync.js
@@ -1,9 +1,9 @@
 // validate
 // WIP branches that need rebasing
 // fetch all remotes like git up
-
 //output: updated branches: [...]
 //        up to date branches: [...]
 //        diverging branches: [...]
 //        no remote branches: [...]
 //recommend fix diverged branches or branches with no remote
+0;
